a2b858371d34bcc147ecc1c23894687f7270e87c
jawed0216 <jawed0216@gmail.com>
2023-07-15 21:03:42 +0530

    structure of project

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const serverConfig = require('./config/server.config');
 
 const app = express();
 
 
-app.listen(3333, ()=>{
-    console.log("server started");
+app.listen(serverConfig.PORT, ()=>{
+    console.log(`server started on the port number ${serverConfig.PORT}` );
 })
