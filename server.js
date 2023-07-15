67b9760bc6941d7733be0fa9917b7fc4c7008627
jawed0216 <jawed0216@gmail.com>
2023-07-15 22:10:53 +0530

    connected to MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const serverConfig = require('./config/server.config');
+const mongoose = required('mongoose');
+const dbConfig = required('./configs/db.config');
 
 const app = express();
+
+mongoose.connect("dbConfig.DB_URL");
+const db = mongoose.connection ;
+
+db.on("error", ()=>{
+    console.log("Error while connecting to DB");
+});
+
+db.once("open", ()=>{
+    console.log("DB is connected");
+})
+
 
 
 app.listen(serverConfig.PORT, ()=>{
